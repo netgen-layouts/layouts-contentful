f725fc1a95995797444abec5e2a8ccc8e61750c2
Edi Modrić <edi.modric@gmail.com>
2017-12-08 13:28:18 +0100

    Rename target handler tags and interfaces

--- lib/Layout/Resolver/TargetHandler/Doctrine/Entry.php
+++ lib/Layout/Resolver/TargetHandler/Doctrine/Entry.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Netgen\BlockManager\Contentful\Persistence\Doctrine\QueryHandler\LayoutResolver\TargetHandler;
+namespace Netgen\BlockManager\Contentful\Layout\Resolver\TargetHandler\Doctrine;
 
 use Doctrine\DBAL\Query\QueryBuilder;
 use Doctrine\DBAL\Types\Type;
-use Netgen\BlockManager\Persistence\Doctrine\QueryHandler\LayoutResolver\TargetHandler;
+use Netgen\BlockManager\Layout\Resolver\TargetHandler\Doctrine\TargetHandlerInterface;
 
-final class Entry implements TargetHandler
+final class Entry implements TargetHandlerInterface
 {
     public function handleQuery(QueryBuilder $query, $value)
     {
