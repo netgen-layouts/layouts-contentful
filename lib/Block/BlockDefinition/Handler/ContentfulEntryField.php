dadabfc4a8e98246e80664b7969642d78ce1d0f0
Edi Modrić <edi.modric@gmail.com>
2020-02-24 09:57:09 +0100

    Use class import

--- lib/Block/BlockDefinition/Handler/ContentfulEntryField.php
+++ lib/Block/BlockDefinition/Handler/ContentfulEntryField.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Netgen\Layouts\Contentful\Block\BlockDefinition\Handler;
 
+use Contentful\RichText\Node\Document;
 use DateTimeInterface;
 
 final class ContentfulEntryField
@@ -48,7 +49,7 @@ final class ContentfulEntryField
             if ($innerField instanceof DateTimeInterface) {
                 $this->value = $innerField;
                 $this->type = self::TYPE_DATETIME;
-            } elseif ($innerField instanceof \Contentful\RichText\Node\Document) {
+            } elseif ($innerField instanceof Document) {
                 $this->value = $innerField;
                 $this->type = self::TYPE_RICHTEXT;
             }
