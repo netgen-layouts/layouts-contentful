2a284d68ed2d840429b9c0ca4c44f771aeb1bbe8
Edi Modrić <edi.modric@gmail.com>
2025-11-28 09:26:55 +0100

    Use JSON_THROW_ON_ERROR constant

--- lib/Service/Contentful.php
+++ lib/Service/Contentful.php
@@ -28,6 +28,8 @@ use function in_array;
 use function json_encode;
 use function sprintf;
 
+use const JSON_THROW_ON_ERROR;
+
 final class Contentful
 {
     /**
@@ -219,7 +221,7 @@ final class Contentful
             $contentfulEntry->setRemoteEntry($remoteEntry);
             $savedCurrentSlug = $this->entrySlugger->getSlug($contentfulEntry);
 
-            $contentfulEntry->setJson((string) json_encode($remoteEntry));
+            $contentfulEntry->setJson((string) json_encode($remoteEntry, JSON_THROW_ON_ERROR));
             $contentfulEntry->setIsPublished(true);
             $contentfulEntry->setIsDeleted(false);
             $this->entityManager->persist($contentfulEntry);
@@ -331,7 +333,7 @@ final class Contentful
     public function refreshSpaceCache(ClientInterface $client): void
     {
         $spacePath = $this->getSpaceCachePath($client);
-        $this->fileSystem->dumpFile($spacePath . '/space.json', (string) json_encode($client->getSpace()));
+        $this->fileSystem->dumpFile($spacePath . '/space.json', (string) json_encode($client->getSpace(), JSON_THROW_ON_ERROR));
     }
 
     /**
@@ -342,7 +344,7 @@ final class Contentful
         $spacePath = $this->getSpaceCachePath($client);
         $contentTypes = $client->getContentTypes();
         foreach ($contentTypes as $contentType) {
-            $this->fileSystem->dumpFile($spacePath . '/ct-' . $contentType->getId() . '.json', (string) json_encode($contentType));
+            $this->fileSystem->dumpFile($spacePath . '/ct-' . $contentType->getId() . '.json', (string) json_encode($contentType, JSON_THROW_ON_ERROR));
         }
     }
 
@@ -405,7 +407,7 @@ final class Contentful
         $contentfulEntry = new ContentfulEntry($remoteEntry);
         $contentfulEntry->setIsPublished(true);
         $contentfulEntry->setIsDeleted(false);
-        $contentfulEntry->setJson((string) json_encode($remoteEntry));
+        $contentfulEntry->setJson((string) json_encode($remoteEntry, JSON_THROW_ON_ERROR));
         $this->entityManager->persist($contentfulEntry);
 
         if (count($this->routeContentTypes) < 1 || in_array($contentfulEntry->getContentType()->getId(), $this->routeContentTypes, true)) {
