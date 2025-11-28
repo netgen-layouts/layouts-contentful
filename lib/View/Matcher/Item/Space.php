9defd3e51a9edef3c6857e8bfa19ce359af9acf2
Edi Modrić <edi.modric@gmail.com>
2025-11-28 20:22:35 +0100

    Use property hooks for view system

--- lib/View/Matcher/Item/Space.php
+++ lib/View/Matcher/Item/Space.php
@@ -19,7 +19,7 @@ final class Space implements MatcherInterface
             return false;
         }
 
-        $entry = $view->getItem()->object;
+        $entry = $view->item->object;
         if (!$entry instanceof ContentfulEntry) {
             return false;
         }
